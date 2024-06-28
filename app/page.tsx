8cc94e7969003ea098e89f24e34d18f316d57d42
odelola <integralandintegrations@gmail.com>
2024-06-28 09:33:25 +0100

    another tortoise commit

--- app/page.tsx
+++ app/page.tsx
@@ -22,30 +22,6 @@ const App = () => {
     onSuccess: (data: any) => {
       alert("Thanks for doing business with us! Come back soon!!"),
         console.log(data);
-    },"use client";
-import React, { useState } from "react";
-import { PaystackButton } from "react-paystack";
-import Image from "next/image";
-
-const App = () => {
-  const publicKey = "pk_test_579b64f84c9afa70dce2fb638479077310a4d8cd";
-  const amount = 1000000;
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-  const [phone, setPhone] = useState("");
-
-  const componentProps = {
-    email,
-    amount,
-    metadata: {
-      name,
-      phone,
-    },
-    publicKey,
-    text: "Pay Now",
-    onSuccess: (data: any) => {
-      alert("Thanks for doing business with us! Come back soon!!"),
-        console.log(data);
     },
 
     onClose: (data: any) => {
@@ -121,52 +97,6 @@ const App = () => {
             className="p-2  bg-blue-500 text-white"
             {...componentProps}
           />
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default App;
-
-
-    onClose: (data: any) => {
-      alert("Wait! Don't leave :(");
-      console.log(data);
-    },
-  };
-
-  return (
-    <div className="flex items-center justify-center w-full h-full">
-      <div className="">
-        <div className="">
-          <div className="">
-            <p>Test</p>
-            <p>{amount}</p>
-          </div>
-        </div>
-        <div className="flex flex-col gap-3">
-          <form className="flex flex-col gap-y-3">
-            <label>Name</label>
-            <input
-              type="text"
-              id="name"
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Email</label>
-            <input
-              type="text"
-              id="email"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label>Phone</label>
-            <input
-              type="text"
-              id="phone"
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </form>
-          <PaystackButton {...componentProps} />
         </div>
       </div>
     </div>
